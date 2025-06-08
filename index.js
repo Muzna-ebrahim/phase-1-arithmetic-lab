bf9354769ffbcf9280175aac4c74f95e83ced2e2
muzna <ebrahimmuznah98@gmail.com>
2025-06-09 01:19:23 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1= 62;
+let num2 = 1;
+const multiply = num1* num2;
+
+let random = Math.floor(Math.random()* 100) + 1;
+
+let num3 =8;
+let num4 = 2;
+const mod = num3 / num4;
+
+const max= Math.max(12,20,5,2);
